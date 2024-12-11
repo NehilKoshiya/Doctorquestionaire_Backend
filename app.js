c010244e8e6287558b8673013185101e8dfa618a
NehilKoshiya <nehilkoshiya1245@gmail.com>
2024-12-11 10:23:59 +0530

    middleware comment

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE PATCH");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE PATCH");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use("/v1", mainRouter);
 app.use(notFoundRoute);
